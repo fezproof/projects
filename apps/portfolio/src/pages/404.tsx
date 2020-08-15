a0098f1e4e78b81a32e860c0ae96dd59ce424d1a
Ben Chidlow <ben.chidlow@woodside.com.au>
2020-08-15 18:58:50 +0800

    Initial setup for gatsby

---
+++ apps/portfolio/src/pages/404.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const NotFound: FC = () => {
+  return <div>404 page not found</div>;
+};
+
+export default NotFound;
---
+++ apps/portfolio/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Index: FC = () => {
+  return <div>this me!</div>;
+};
+
+export default Index;

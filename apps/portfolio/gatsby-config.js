880e8c3bc01b4f1cea7f30af8dd05b62aff41be1
Ben Chidlow <ben.chidlow@woodside.com.au>
2020-08-15 19:14:39 +0800

    Added offline support Added web manifest

--- apps/portfolio/gatsby-config.js
+++ apps/portfolio/gatsby-config.js
@@ -15,5 +15,23 @@ module.exports = {
         allExtensions: true, // defaults to false
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ben Chidlows Portfolio`,
+        short_name: `BenC Portfolio`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      // options: {
+      //   precachePages: [`/about-us/`, `/projects/*`],
+      // },
+    },
   ],
 };

53c8291f78cae037abf895854aecae9f599f3cef
Ben Chidlow <ben.chidlow@woodside.com.au>
2020-08-19 17:42:44 +0800

    added basic 404 page

--- apps/portfolio/src/pages/404.tsx
+++ apps/portfolio/src/pages/404.tsx
@@ -1,8 +1,33 @@
 import React, { FC } from "react";
 import BasicLayout from "../layouts/BasicLayout";
+import {
+  Flex,
+  IllustratedMessage,
+  Heading,
+  Content,
+} from "@adobe/react-spectrum";
+import NotFound from "@spectrum-icons/illustrations/NotFound";
 
-const NotFound: FC = () => {
-  return <BasicLayout title="No page found">404 page not found</BasicLayout>;
+const PageNotFound: FC = () => {
+  return (
+    <BasicLayout title="No page found">
+      <Flex
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <IllustratedMessage>
+          <NotFound />
+          <Heading>Error 404: Page not found</Heading>
+          <Content>
+            This page isn't available. Try checking the URL or visit a different
+            page.
+          </Content>
+        </IllustratedMessage>
+      </Flex>
+    </BasicLayout>
+  );
 };
 
-export default NotFound;
+export default PageNotFound;

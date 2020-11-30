2fe31947262aa27f56d0829c8ab0d2c510b43575
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2020-12-01 00:54:49 +0800

    Can now look nice and be printed

--- apps/portfolio/src/pages/resume.tsx
+++ apps/portfolio/src/pages/resume.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Grid } from '@chakra-ui/react';
+import { Box, Container, Grid, useMediaQuery } from '@chakra-ui/react';
+
 import ResumeAside from 'src/components/asides/ResumeAside';
 import ResumeFooter from 'src/components/footers/ResumeFooter';
 import ResumeHeader from 'src/components/headers/ResumeHeader';
@@ -6,18 +7,32 @@ import ResumeMain from 'src/components/mains/ResumeMain';
 import ResumeLayout from 'src/layouts/ResumeLayout';
 
 const Resume = () => {
+  const [isPrint] = useMediaQuery('print');
   return (
     <ResumeLayout>
       <Container maxW="lg" p="1rem">
         <Grid
-          templateAreas={`
-            "header main"
-            "aside main"
-            "aside footer"
-          `}
-          gridTemplateColumns="15rem auto"
+          gridTemplateAreas={[
+            isPrint
+              ? `
+                  "header main"
+                  "aside main"
+                  "aside footer"
+                `
+              : `
+                  "header"
+                  "main"
+                  "aside"
+                  "footer"
+                `,
+            `
+              "header main"
+              "aside main"
+              "aside footer"
+            `,
+          ]}
+          gridTemplateColumns={[isPrint ? '15rem auto' : 'auto', 'auto auto']}
           gap="1rem"
-          gridAutoRows="min-content"
           h="100%"
         >
           <Box as="header" gridArea="header">

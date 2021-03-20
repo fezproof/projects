6135c7bcf06f348ce6827f581e55d8e5031af662
Ben Chidlow <26830309+fezproof@users.noreply.github.com>
2021-03-21 01:42:36 +0800

    WIP

--- apps/dnd-graphql/src/functions/graphql/handler.ts
+++ apps/dnd-graphql/src/functions/graphql/handler.ts
@@ -2,15 +2,50 @@ import { ApolloServer, gql } from 'apollo-server-lambda';
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Block {
+    id: ID!
+    content: String
+    children: [Block]
+    page: Page!
+  }
+
+  type Page {
+    id: ID!
+    blocks: [Block]
+    slug: String!
+    snippet: Block
+  }
+
   type Query {
-    hello: String
+    page(id: ID!): Page
+    pages: [Page]
+
+    block(id: ID!): Block
+
+    # user...
   }
 `;
+
+const blocks = [
+  { id: 'block-1', content: 'This is some content to render', pageId: '1' },
+];
+
+const pages = [{ id: '1', slug: 'human' }];
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    page: (parent, { id }, context, info) =>
+      pages.find((page) => page.id === id),
+
+    pages: () => pages,
+    block: (_, { id }) => blocks.find((block) => block.id === id),
+  },
+  Page: {
+    blocks: ({ id }) => blocks.filter((block) => block.pageId === id),
+  },
+  Block: {
+    page: ({ pageId }) => pages.find((page) => page.id === pageId),
   },
 };
 
